4e565ce6aa37248ab6e45829d2b1675def75200a
SahithiAekka <sahithiaekka0261@gmail.com>
2025-04-26 13:35:27 -0400

    add

--- lrnjson.py
+++ lrnjson.py
@@ -7,6 +7,7 @@ person = {
     "isStudent": True
 }
 
+
 # Convert Python object to JSON string
 json_string = json.dumps(person)
 print("JSON String:", json_string)
@@ -15,3 +16,17 @@ print("JSON String:", json_string)
 python_object = json.loads(json_string)
 print("\nPython Object:", python_object)
 print("Type:", type(python_object))
+
+restraunt ={
+    "name": "Noori pocha",
+    "cuisineType": "Korean",
+    "popularDishes": ["chickenbulgogi", "fried chicken", "soju"]
+}
+
+with open('restraunt.json', 'w') as file:
+    json.dump(restraunt, file, indent=4)
+    
+with open('restraunt.json','r') as file:
+    loaded_data = json.load(file)
+    print("\nLoaded Data", loaded_data)
+    
