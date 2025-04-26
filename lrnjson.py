a43a2b214e305e3acd7fd8be1585fbf05185ec3e
SahithiAekka <sahithiaekka0261@gmail.com>
2025-04-26 13:05:42 -0400

    add more concepts

--- lrnjson.py
+++ lrnjson.py
@@ -9,4 +9,9 @@ person = {
 
 # Convert Python object to JSON string
 json_string = json.dumps(person)
-print(json_string)
+print("JSON String:", json_string)
+
+# Convert back to Python object
+python_object = json.loads(json_string)
+print("\nPython Object:", python_object)
+print("Type:", type(python_object))
