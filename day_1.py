3de40cb9d04148071e78652b09e5fec68d98c2e0
SahithiAekka <sahithiaekka0261@gmail.com>
2025-04-29 11:37:06 -0400

    Print func, Input func

--- day_1.py
+++ day_1.py
@@ -1,6 +1,6 @@
 # Print function 
 print("Hello World")
-
+print("Hello world!\nHello world!\nHello world")
 print("I am Sahithi Aekka\nI live in Michigan")
 
 # string concatenation
@@ -12,6 +12,19 @@ print("I am a cloud engineer"+", although I love cooking and want to be a chef")
     # pycharm is an editor like VScode, cursor 
 
 # Input function
-input("Why do I want to be a chef?")
 print("Thank you if you belive in me! and if you don't its okay your validation means nothing to me " + input("why do I want to be a chef"))
- 
+print("Hello "+input("what is your name?")+"!")
+name = input("What is your name?")
+print(len(name))
+
+print(len(input("what's your name?")))
+
+username = input("what's your name?")
+length = len(username)
+print(length)
+
+# BAnd name generator
+print("Hello, welcome to the band generator project")
+city=input("which city did you grow up in")
+pet=input("what is your favourite dish?")
+print("Your band name is "+ city + " " + pet)
