b4cb4c50df3720ac7bd8fa9d3ae19f4b87bd8633
SahithiAekka <sahithiaekka0261@gmail.com>
2025-04-29 16:02:19 -0400

    data types, operators, round func, assignment operato

---
+++ day_2.py
@@ -0,0 +1,58 @@
+# Primitive Data Types
+print(len("123498646282"))
+
+print("Hello"[-3])
+
+
+#string
+print("123"+"456")
+
+#int 
+print(123+456)
+
+#Large integers 
+#there's a diff btw _ and . the poit mnakes the number a float
+print(123_456_789)
+
+#Floating point number 
+print(123.456)
+
+#Boolean 
+print(True)
+False 
+
+#type checking 
+print(type("123")) 
+
+#type casting 
+print(int("123")+int("456"))
+# now the computer will consider these as integers becuse we have converted their 
+
+# Rounding a Number
+# However, if you want to round a decimal number to the nearest whole number using the traditional mathematical way, where anything over .5 rounds up and anything below rounds down. Then you can use the python round() function.
+
+# round(3.738492) # Becomes 4
+
+# round(3.14159) # Becomes 3
+
+# round(3.14159, 2) # Becomes 3.14
+
+# Assignment Operators
+# Assignment operators such as the addition assignment operator += will add the number on the right to the original value of the variable on the left and assign the new value to the variable.
+
+# +=
+
+# -=
+
+# *=
+
+# /=
+
+# f-Strings
+# In Python, we can use f-strings to insert a variable or an expression into a string.
+
+# age = 12
+
+# print(f"I am {age} years old")
+
+# # Will output I am 12 years old.
