e55856e43894974a2d45f6bc10c6ca028d4066e5
SahithiAekka <sahithiaekka0261@gmail.com>
2025-04-28 20:17:37 -0400

    add day 1 python progress

---
+++ day_1.py
@@ -0,0 +1,16 @@
+# Print function 
+print("Hello World")
+
+print("I am Sahithi Aekka\nI live in Michigan")
+
+# string concatenation
+print("I am a cloud engineer"+", although I love cooking and want to be a chef")
+
+    # indentation errors can also occur  so spacing is very important 
+    # to make any line into a commemt press "ctrl" + "/" at the same time 
+    # to adjust the indentation, press "ctrl" + "]" at the same time 
+    # pycharm is an editor like VScode, cursor 
+
+# Input function
+input("Why do I want to be a chef?")
+print("Thank you if you belive in me! and if you don't its okay your validation means nothing to me " + input("why do I want to be a chef"))
