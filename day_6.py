df2325719dd8db60f3d13eaef3bead5810a92904
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-06 12:16:48 -0400

    add reborg maze

--- day_6.py
+++ day_6.py
@@ -50,3 +50,36 @@ def fun_1():
 fun_1()
 
 # spaces or tab?? for indentation : They say spaces are better 
+
+# While loop : loop that will continue going while the condition is true 
+
+# Hurdles race
+# Reeborg has entered a hurdle race, but he does not know in advance how long the race is. Make him run the course, following a path similar to the one shown, but stopping at the only flag that will be shown after the race has started.
+# What you need to know
+# The functions move() and turn_left().
+# The condition at_goal() and its negation.
+# How to use a while loop.
+# Your program should also be valid for world Hurdles 1.
+
+# Difficulty level
+
+
+
+# Reborg maze 
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while not wall_in_front():
+    move()
+turn_left()
+
+while not at_goal():
+    if not wall_on_right():
+        turn_right()
+        move() 
+    elif not wall_in_front():
+        move()
+    else:
+        turn_left()
