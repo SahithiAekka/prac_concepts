65c22919d2a0a42888cbd56f96da993c3facee54
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-08 12:28:01 -0400

    dictonaries, nesting, bidding project

--- day_9.py
+++ day_9.py
@@ -47,4 +47,52 @@ print(travel_log["France"][2])
 nested_list=["a","b",["c","d"]]
 
 
-print(nested_list[2][1])
+# print(nested_list[2][1])
+
+# Max function 
+fruits = {"apple":2, "banana":4, "strawberry":25}
+print(max(fruits, key=fruits.get))
+
+
+print("######################   Auction ##############################")
+
+logo = r'''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+
+print(logo)
+
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
