6b790dc7fcb0829e740e7d4bf6d2e4711f771372
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-13 11:35:28 -0400

    Debugging

--- day_13.py
+++ day_13.py
@@ -46,3 +46,31 @@ except ValueError:
     
 if age < 18:
     print(f"You can't drive at the age of {age}")
+    
+
+# *******************************************  How to use a debugger **********************************************************************
+# Breakpoint -- breaking line higlighted . puts a break on code at that particular line then you go to the debug run mode 
+# step over button - execute code line by line  
+# step into button - execute code line ny line , shows how built in function in python work. 
+# ignore library function written by python  you can use step over button 
+# step out - exit debugging mode 
+# two tabs: thread and variables-- shows values of varibales in the code through out 
+        #  console
+
+# Most IDEs (Intelligent Development Environments) such as PyCharm will have built-in tools for debugging. 
+# This is normally known as the debugger. In many ways, they are like print statements on steroids.
+
+# Debuggers allows us to peek into our code during execution and pause on chosen lines to figure out what is the inner mechanism and where it's going wrong.
+
+# There are a couple of things that are the same in most IDEs which you should be familiar with:
+
+# Breakpoint - You can set a breakpoint by clicking on a line in the gutter of the code (where the line numbers are). 
+#              This line will be where the program pauses during debug run.
+
+# Step Over - This button will go through the execution of your code line by line and allow you to view the intermediate values of your variables.
+
+# Step Into - This will enter into any other modules that your code references. e.g. 
+#             If you use a function from the random module it will show you the original code for that 
+#             function so you can better understand its functionality and how it relates to your problems.
+
+# Step Into My Code - This does the same thing as Step Into, but it limits the scope to your own project code and ignores library code such as random
