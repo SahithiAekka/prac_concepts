49846fc0b5731d660dbbc673712ca75da57f40a4
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-17 23:52:22 -0400

    Coffee problem

--- day_15.py
+++ day_15.py
@@ -88,6 +88,10 @@ while is_on:
             payment = process_coins()
             if is_transaction_successful(payment, drink["cost"]):
                 make_coffee(choice, drink["ingredients"])
+                
+                
+                
+                
 
 
 
