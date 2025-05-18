0def15737659eeb124cc526c83433c6c7a6b16cf
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-17 23:52:46 -0400

    OOPS concepts

--- day_16.py
+++ day_16.py
@@ -1,1 +1,15 @@
 # OOPS 
+# Procedural programing - earliest method but the increase in complexity has made it hard to mantain 
+# Object oreiented progarm -- SPlit a larger tasks into smaller tasks which are reusable 
+# reusable modules  scalable , sipmlyfy the relationships
+# trying to model a real world object  
+# while trying to model a waiter you have to thk of two things : what the waiter HAS ------ ATTRIBUTE  = variable taht is attached to a articular object 
+
+                                                            #    what the waiter DOES ----- METHODS = modeled by fucntion \
+# we can genearate  multiple versions of the waiter like John , Betty using something called as a blueprint
+
+# over here the          waiter(CLASS )               John, Betty  (OBJECTS )
+                                                     
+# from the blue print which is the class we can generate as many objects as we want                                                      
+
+# object--> car = Carblueprint() <-- class = uses Pascal case                                                              
