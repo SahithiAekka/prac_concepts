5e8c2668bcdb1ab4881af8369065ce1938559978
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-31 14:02:44 -0400

    Class, object, constructor, attributes, method

--- day_17.py
+++ day_17.py
@@ -1,20 +1,28 @@
 class User:
-    def __init__(self,id,name):
-        print("new user being created...")
+    def __init__(self,id,name): 
+        # a constructor sets up what variables should go in the class without errors. 
+        # you can sometimes give default values to the variebles 
+         
         self.id= id
         self.username= name
         self.followers = 0
         self.following = 0
         
     def follow(self,user):
+        user.followers +=1
+        self.following +=1
        
 
 # user_1= User()
 # user_1.id = "001"
 # user_1.name= "Sahithi"
 
+# how to specify what should happen when your obejct is being created 
+# iniatlizing the or cretating the starting values for the  this helps in not having errors by mistakely changing while creating users 
+
 
 user_1=User("001","Sathvik")
+
 # Attribbute is a variable that is associated with the object 
 
 # PascalCase -- used for class names 
@@ -22,16 +30,16 @@ user_1=User("001","Sathvik")
 # snake_case -- used for all most everything 
 
 
-# how to specify what should happen when your obejct is being created 
-# iniatlizing the or cretating the starting values for the 
-
-print(user_1.name)
-print(user_1.id)
-
 # ATTRRIBUTES ARE THE THINGS THAT THE OBJECT HAS AND METHODS ARE THE THINGS THAT THE OBJECT DOES
-# WHEN A FUNCTION IS ATTACHED TO AN OBJECT THEN ITS CALLED A METHOD 
+# WHEN A FUNCTION IS ATTACHED TO AN OBJECT THEN ITS CALLED A METHOD it need a self 
 
-# created a method secret name 
-User.follow("user_2","002","Jatin")
+user_2=User("002", "Jatin")
 
-print()
+print(user_1.following)
+print(user_2.followers)
+
+user_1.follow(user_2)
+
+
+print(user_1.following)
+print(user_2.followers)
