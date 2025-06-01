f2a5d528e3338ddc86f7ea830500daf05d0793a3
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-31 20:18:28 -0400

    quiz game

---
+++ quiz-game-start/test_1.py
@@ -0,0 +1,18 @@
+# question_data={
+#     "text1": "hello", 
+#     "answer1":"Sahithi",
+#     "text": "A few ounces of chocolate can to kill a small dog.", 
+#     "answer": "True"
+#     }
+
+# for q in question_data:
+#     print(q)
+#     print(question_data[q])
+    
+    
+a= 5
+def func1():
+    return a>7 
+        
+print(func1())
+       
