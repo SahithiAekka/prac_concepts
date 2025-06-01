ac6d45fcff550ee3c2e52a5745954ded62e2ad9d
SahithiAekka <sahithiaekka0261@gmail.com>
2025-05-31 20:19:25 -0400

    OOPS intermediate

--- day_16.py
+++ day_16.py
@@ -3,7 +3,7 @@
 # Object oreiented progarm -- SPlit a larger tasks into smaller tasks which are reusable 
 # reusable modules  scalable , sipmlyfy the relationships
 # trying to model a real world object  
-# while trying to model a waiter you have to thk of two things : what the waiter HAS ------ ATTRIBUTE  = variable taht is attached to a articular object 
+# while trying to model a waiter you have to thk of two things : what the waiter HAS ------ ATTRIBUTE  = variable that is attached to a articular object 
 
                                                             #    what the waiter DOES ----- METHODS = modeled by fucntion \
 # we can genearate  multiple versions of the waiter like John , Betty using something called as a blueprint
