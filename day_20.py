40f0035d868af436585f1648a6b44a644f1e2928
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-02 12:33:59 -0400

    Snake game planing

---
+++ day_20.py
@@ -0,0 +1,11 @@
+# ***** SNAKE GAME *****
+
+# Breaking down the problem : seven steps 
+
+# Step 1 : create the snake 
+# Step 2: Move the snake 
+# Step 3: Control the snake 
+# Step 4: Detect collion with food 
+# Step 5: Create a scoreboard
+# Step 6: Detect collision with wall 
+# Step 7: Detect collision with tail 
