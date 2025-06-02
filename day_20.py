3e56df08b2cb107b4e5bf9ec7b3673dc836707b9
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-02 14:49:54 -0400

    snake game

--- day_20.py
+++ day_20.py
@@ -9,3 +9,7 @@
 # Step 5: Create a scoreboard
 # Step 6: Detect collision with wall 
 # Step 7: Detect collision with tail 
+
+# screen.update() updtaes the screen and makes it better looking 
+# time.sleep(0.1) # delay 0.1 sec and refresh the screen 
+#  in python for constants are used by captiol letters 
