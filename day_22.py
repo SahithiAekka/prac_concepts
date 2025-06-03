16cf53f7709d2d963983df352d77285838d64b24
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-03 17:45:03 -0400

    Ping pong game planning

---
+++ day_22.py
@@ -0,0 +1,12 @@
+# PONG Game 
+
+# Step 1: Create the screen 
+# Step 2: Create and move a paddle
+# Step 3: Create another paddle 
+# Step 4: Create the ball and make it move 
+# Step 5: Detect  collision with wall and bounce 
+# Step 6: Detect  collision with paddle 
+# Step 7: Detect when paddle misses 
+# Step 8: Keep score 
+
+# additional step : when the player goes above the scree point to the other person from refree 
