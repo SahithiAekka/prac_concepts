89c2e7896765da901328a01b3d96117df1cd558a
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-06 13:54:45 -0400

    Pandas, CSV , Data frame , Series

---
+++ day_25.py
@@ -0,0 +1,27 @@
+# How to work woth CSV files, Pandas 
+
+# Reading data files : CSV _ Comma Seperated Values 
+
+# To install pandas : first create virtual env 
+                        # python -m venv myvenv
+                    # activating virtual env 
+                        # myvenv/script/activate
+                    # installing pacakge name 
+                        # pip install package_name -> pandas
+                    # Requiremt text - you need to create a txt file and give in the requiremts then do 
+                        # pip install -r req.txt
+                    #  checking if you've installed pandas/ any library 
+                        # pip list 
+
+                        
+ 
+# Pandas: give beautifully formated data it makes it easier and simpler 
+# two primary data structures of pandas-- Series (1 dimensional)
+                                        # DataFrame(2 dimensional)
+
+# Dataframe is equivalent to a excel sheet or the whole table 
+
+# Series is like a list 
+
+# data.to_csv("new_data.csv") --> this creates a new csv file in the folder that you. 
+
