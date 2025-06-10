0e46964776bbfc805df508ad756b419ccaf08ddf
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-10 12:45:16 -0400

    building a pomodoro app, canvas

---
+++ day_28.py
@@ -0,0 +1,13 @@
+# Day 28 - building a pomodoro app - francesco cilliro 
+
+# Canvas widget allows you to layer things on top of each other 
+# Tkinter uses different layout managers: pack(), place(), grid()
+
+# PhotoImage() reads the image file in a Tkinter-compatible format.
+
+# canvas.create_image(x, y, image=...) places the image at the center.
+
+# X and Y are coordinates relative to the canvas.
+
+
+# Event driven programs 
