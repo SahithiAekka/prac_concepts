8cf75fbe0a3c6fc044c653c0a711e8815a07539a
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-12 23:11:44 -0400

    add try and except from day 30

--- nato_day26/main.py
+++ nato_day26/main.py
@@ -1,29 +1,18 @@
 import pandas
 
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
 
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
+def generate_phonetic ():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+generate_phonetic()
