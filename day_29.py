b79c347e227d97f6a16b88ef47ae9256f1e6706f
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-12 21:50:19 -0400

    Passowrd Manager App

---
+++ day_29.py
@@ -0,0 +1,11 @@
+# Day 29  - building a password manager 
+
+# use grid and column space 3 column and 5 
+
+# GRID COLUMN SPAN 
+
+# saving entry information into a txt file 
+
+# Message box module : Ask the user for input or any information 
+
+pyper_clip = " Python  module for copy and pasting on clipboard "
