a4ee3b49db5f02b5602cb8a890d57796c00f8fbf
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-16 11:48:33 -0400

    flash card for learning languages

--- day_31.py
+++ day_31.py
@@ -1,1 +1,9 @@
 # Day31- Bulding a flashcard project for learning languages 
+
+
+# Step 1 : create UI with tkinter 
+# Step 2 : create new flash cards
+# Step 3 : Flip the cards 
+# Step 4 : Save your progress 
+
+
