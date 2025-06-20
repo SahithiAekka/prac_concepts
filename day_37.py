8ed431964d0913c51961796d9ea2d9aefc9ca960
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-20 17:00:45 -0400

    API- POST, PUT, DELETE

---
+++ day_37.py
@@ -0,0 +1,9 @@
+# GET -- get data from an API 
+# POST -- we give the external system data 
+# PUT -- update a piece of data 
+# DELETE -- just to delete 
+
+
+
+
+# https://pixe.la/v1/users/sahithiaekka/graphs/graph1.html 
