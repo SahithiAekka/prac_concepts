e3e86394083e9ef9c64c953d416b099d9b3f9c84
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-20 17:15:53 -0400

    HTML - basics

---
+++ day_41.py
@@ -0,0 +1,15 @@
+# HTML _ HYPER TEXT MARK UP LANGUAGE 
+
+# 🧭 How Does a Website Load?
+# You type google.com into your browser.
+
+# Your browser contacts your ISP (Internet Service Provider) (e.g., Comcast, BT).
+
+# The ISP forwards your request to a DNS server (Domain Name System).
+
+# The DNS server acts like a phonebook and returns the IP address for google.com.
+
+# Your browser uses that IP address to send a request to the Google server.
+
+# The Google server responds with all the data/files needed to show the homepage.
+
