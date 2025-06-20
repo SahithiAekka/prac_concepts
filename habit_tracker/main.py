9efa83e2d92bd4ad28d528a7faa9ac49f2f9e19d
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-20 16:59:35 -0400

    Pixela Habit tracker created using API documentation- PUT POST DELETE

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 
@@ -38,16 +39,41 @@ headers = {"X-USER-TOKEN" :token_key }
 # response=requests.post(url= graph_endpoint, json=graph_config, headers=headers)
 # print(response.text) #{"message":"Success.","isSuccess":true}
 
+today= datetime(year=2025, month=6, day=20)
+# print(today)
+
 
 
 pixel_craetion_endpoint= f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 #create a request body 
 pixel_data={
-    "date": "20250620",
-    "quantity":"1",
+    "date":today.strftime("%Y%m%d"),
+    "quantity":"4",
 }
 
-# ...........creat pixel data.....
-response=requests.post(url=pixel_craetion_endpoint, json=pixel_data, headers=headers)
-print(response.text)
-# {"message":"Success.","isSuccess":true}-- output 
+# # ...........creat pixel data.....
+# response=requests.post(url=pixel_craetion_endpoint, json=pixel_data, headers=headers)
+# print(response.text)
+# # {"message":"Success.","isSuccess":true}-- output 
+# strftime- string f time method 
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+## PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+# {"message":"Success.","isSuccess":true}- Output 
+
+# delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+
+## DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
+
+
+# https://pixe.la/v1/users/sahithiaekka/graphs/graph1.html----- URL 
