9434661307d87eb5059d567e80282ad07e331f4d
SahithiAekka <sahithiaekka0261@gmail.com>
2025-06-23 22:34:35 -0400

    Day 41- HTML basics

--- day_41.py
+++ day_41.py
@@ -19,3 +19,54 @@
 # Dynamic IP	Changes periodically. Common for home networks.
 # Public IP	The address your ISP assigns to you on the internet.
 # Private IP	Used inside your home network (e.g., 192.168.1.1).
+
+
+
+# HTML stands for HyperText Markup Language.
+
+# It's the core language used to structure and define the content of a website.
+# While modern websites use HTML + CSS + JavaScript, only HTML is required to create a basic functioning web page.
+
+# *****HyperText**********
+# HyperText refers to clickable links (hyperlinks) that connect one HTML page to another.
+# This forms the navigational structure of the web.
+
+# ******Markup Language*********
+# A markup language uses tags to annotate content.
+# Tags act like instructions (e.g., make this text a heading, a paragraph, a link).
+# Similar to how editors mark up text with squiggles or lines in manuscripts.
+
+# *********** HTML Tags**********
+# Tags are written as <tagname>...</tagname>.
+# Early HTML had few tags; modern HTML has many, but only a core set is used regularly.
+# Examples: 
+# <h1> to <h6> for headings
+# <p> for paragraphs
+
+
+# HTML Tag  
+# Tags: Code between angle brackets, e.g., <h1> or </h1>.
+# Opening tag: <h1>
+# Closing tag: </h1> (note the /)
+
+# Element: The complete block – includes opening tag, content, and closing tag.
+
+# Example: <h1>Hello World</h1> is an element.
+
+# Heading Levels
+# HTML has six levels of headings: <h1> to <h6>.
+# <h1> is the most important (usually the page or section title).
+# <h6> is the least important.
+# There is no <h7> – <h6> is the deepest defined level.
+
+
+#Structure and Semantics
+# Use headings like a table of contents or book structure:
+# <h1>: Book title
+# <h2>: Chapter titles
+# <h3>: Sections in chapters
+# <h4>: Subsections or diagrams
+
+
+# Visual size decreases from <h1> to <h6>, but can be styled with CSS.
+
