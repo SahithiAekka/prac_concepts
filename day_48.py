587ec849da6912fa9e0f63d43a8f7776fc26dc90
SahithiAekka <sahithiaekka0261@gmail.com>
2025-07-02 17:15:53 -0400

    Getting started using selenium

---
+++ day_48.py
@@ -0,0 +1,12 @@
+from selenium import webdriver 
+
+# Keepin gchrome browser open after program finishes 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+# Setting up with selenium 
+driver= webdriver.Chrome(options=chrome_options)
+driver.get(url="https://www.amazon.com")
+
+# driver.close() # closes the active tab  
+driver.quit() #closes the entire browser 
