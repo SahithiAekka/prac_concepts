8e3e5a719c0f8508691476db76d8619cc867a6b8
SahithiAekka <sahithiaekka0261@gmail.com>
2025-07-02 15:26:53 -0400

    Movies to watch we scraping beautiful soup

--- movies_to_watch/main.py
+++ movies_to_watch/main.py
@@ -5,4 +5,30 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+website_html = response.text
 
+soup = BeautifulSoup(website_html, "html.parser")
+
+# print(soup.prettify())
+
+
+# scraping the H3 element. use google inspection 
+
+all_movies= soup.find_all(name="h3", class_="title")
+# print(all_movies)
+
+movie_titles= [movie.getText() for movie in all_movies]
+# print(movie_titles)
+movies = movie_titles[::-1]
+
+
+
+# for n in range(len(movie_titles) -1,-1,-1):
+#     print(movie_titles[n])
+
+# print(movies)
+with open("movies.txt", mode="w", encoding="utf-8") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
+        
