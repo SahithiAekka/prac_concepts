10bbd21e0eeebc6633b30f0b339b61393b720b56
SahithiAekka <sahithiaekka0261@gmail.com>
2025-07-06 02:19:09 -0400

    Web Dev - full stack

---
+++ day_54.py
@@ -0,0 +1,23 @@
+#  Web Devlopemyt using Flask 
+
+# Frontend - display to the user 
+#            Languages ----------- HTML , CSS , JavaScript 
+#            Framework ----------- Angular, React 
+
+# Backend - logic fo rthe app tio run smoothly 
+#           Languages ------------------ Java, Javascriot, Python, Ruby, React 
+#           Framework ------------------ Flask, Django 
+
+# Frameworks are tools that come with a lot of code prebuilt for a lot of functionalities that you find when building 
+
+# For python as the backend FLASK, DJANGO, BOTTLE, CHERRY PIE, PYRAMID 
+
+# Flask - begginers and small projects 
+# Django - larger commercial projects 
+
+# Backend -- Client    +     Server    +     Database 
+
+# client - a person going on the browser part that faces the user 
+# server - a powerful computer hoked up to internet 24/7
+# database - like a spreadsheet 
+
