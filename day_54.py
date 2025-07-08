71b227a46e7e1df1b6e6afb69dfc7583ac990059
SahithiAekka <sahithiaekka0261@gmail.com>
2025-07-08 14:30:24 -0400

    Web Devlopemyt using Flask

--- day_54.py
+++ day_54.py
@@ -25,3 +25,21 @@
 
 # Library- you call upon when you want to use it means you're in control 
 # Framework - you have to abide their rules 
+
+# Flask - "https://flask.palletsprojects.com/en/stable/quickstart/" starting doc 
+
+# Command line -  interact with your computer by typing one-line commands
+
+# Kernel - refers to actual program that interface with the hardwar 
+
+
+# The shell is the interface between you (the user) and the kernel --the core of the OS that interacts with hardware. 
+
+# Command 	        Purpose 	                                        Example / Notes
+# pwd	            Print working directory (shows current location)	
+# ls	            List files/folders in current directory	
+# cd <dir>	        Change directory	                                Use cd .. to go up one level
+# mkdir<name>	    Make a new folder	
+# touch<filename>	Create a new file	E.g., touch main.py
+# rm <filename>	    Delete a file	
+# rm -rf<foldername>Force delete a folder and everything inside	        ⚠️ Dangerous — use with caution!
